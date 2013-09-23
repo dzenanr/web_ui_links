5d0815967a07cb5c729cab06192e450847e678d9
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-09-23 16:50:36 -0400

    use .. in links.add

--- web_links_s04/web/web_links.dart
+++ web_links_s04/web/web_links.dart
@@ -12,7 +12,8 @@ void main() {
   var link1 = new Link('On Dart', 'http://ondart.me/');
   var link2 = new Link('Web UI', 'http://www.dartlang.org/articles/web-ui/');
   var link3 = new Link('Books To Read', 'http://www.goodreads.com/');
-  links.add(link1);
-  links.add(link2);
-  links.add(link3);
+  links
+    ..add(link1)
+    ..add(link2)
+    ..add(link3);
 }
